7afedbd8303d3466fce97f72c697e4fa52d06c54
Emre <aemr3@users.noreply.github.com>
2018-12-25 12:18:01 +0300

    Update 14-nginx-real-ip.sh

--- files/config/init/14-nginx-real-ip.sh
+++ files/config/init/14-nginx-real-ip.sh
@@ -4,7 +4,7 @@ REAL_IP_SOURCE_CONF="/config/init/http-real-ip.conf"
 REAL_IP_TARGET_CONF="/etc/nginx/addon.d/http-real-ip.conf"
 
 if [ "${NGINX_ENABLE_REAL_IP^^}" = TRUE ]; then
-  mv $REAL_IP_SOURCE_CONF > $REAL_IP_TARGET_CONF
+  cat $REAL_IP_SOURCE_CONF > $REAL_IP_TARGET_CONF
   sed -i "s#0.0.0.0/0#${NGINX_REAL_IP_FROM}#" $REAL_IP_TARGET_CONF
   sed -i "s#X-Forwarded-For#${NGINX_REAL_IP_HEADER}#" $REAL_IP_TARGET_CONF
 fi
